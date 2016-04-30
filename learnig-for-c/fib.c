a23f86263b71fc567c11c188522a89884943fd7b
Unflypig <1085025884@qq.com>
2016-04-30 11:27:21 +0800

    增加：倒序打印斐波那契数列

--- learnig-for-c/fib.c
+++ learnig-for-c/fib.c
@@ -2,11 +2,27 @@
 
 int main()
 {
+    //斐波那契数列
     int fibarry[10]={1,1};
     for (int i=2; i<10; i++)
         fibarry[i] = fibarry[i-1] + fibarry[i-2];
     for (int i=0; i<10; i++)
         printf("fibarry[%d] = %d\n", i, fibarry[i]);
 
+    //倒序输出
+    int arrstart = 0;
+    int arrstop = sizeof(fibarry)/sizeof(fibarry[0]) - 1;
+    printf("sizeof fibarry = [%d]\n", arrstop);
+    int tmp;
+    while(arrstart < arrstop)
+    {
+        tmp = fibarry[arrstop];
+        fibarry[arrstop] = fibarry[arrstart];
+        fibarry[arrstart] = tmp;
+        arrstart ++;
+        arrstop --;
+    }
+    for (int i=0; i<10; i++)
+        printf("fibarry[%d] = %d\n", i, fibarry[i]);
     return 0;
 }
